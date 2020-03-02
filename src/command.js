603d1980690d84991fb350789756e6d3a1343659
Jonas Amundsen <jonasba@gmail.com>
2020-03-02 15:59:54 +0100

    Add retry-ability by using verifyUpcomingAssertions
    This method is unforunately somewhat undocumented, but referred to in
    issues [1] and used in official plugins [2].
    
    [1] https://github.com/cypress-io/cypress/issues/2670
    [2] https://github.com/cypress-io/cypress-xpath/blob/v1.4.0/src/index.js#L120-L122

--- src/command.js
+++ src/command.js
@@ -19,49 +19,63 @@ export function matchImageSnapshotCommand(defaultOptions) {
       ...((typeof maybeName === 'string' ? commandOptions : maybeName) || {}),
     };
 
-    cy.task(MATCH, {
-      screenshotsFolder,
-      updateSnapshots,
-      options,
-    });
+    const getValue = () => {
+      cy.task(MATCH, {
+        screenshotsFolder,
+        updateSnapshots,
+        options,
+      });
 
-    const name = typeof maybeName === 'string' ? maybeName : undefined;
-    const target = subject ? cy.wrap(subject) : cy;
-    target.screenshot(name, options);
+      const name = typeof maybeName === 'string' ? maybeName : undefined;
+      const target = subject ? cy.wrap(subject) : cy;
+      target.screenshot(name, options);
 
-    return cy
-      .task(RECORD)
-      .then(
-        ({
-          pass,
-          added,
-          updated,
-          diffSize,
-          imageDimensions,
-          diffRatio,
-          diffPixelCount,
-          diffOutputPath,
-        }) => {
-          if (!pass && !added && !updated) {
-            const message = diffSize
-              ? `Image size (${imageDimensions.baselineWidth}x${
-                  imageDimensions.baselineHeight
-                }) different than saved snapshot size (${
-                  imageDimensions.receivedWidth
-                }x${
-                  imageDimensions.receivedHeight
-                }).\nSee diff for details: ${diffOutputPath}`
-              : `Image was ${diffRatio *
-                  100}% different from saved snapshot with ${diffPixelCount} different pixels.\nSee diff for details: ${diffOutputPath}`;
+      return cy.task(RECORD);
+    };
 
-            if (failOnSnapshotDiff) {
-              throw new Error(message);
-            } else {
-              Cypress.log({ message });
-            }
+    const resolveValue = () => {
+      return getValue().then(value => {
+        return cy.verifyUpcomingAssertions(
+          value,
+          {},
+          {
+            onRetry: resolveValue,
+          }
+        );
+      });
+    };
+
+    return resolveValue().should(
+      ({
+        pass,
+        added,
+        updated,
+        diffSize,
+        imageDimensions,
+        diffRatio,
+        diffPixelCount,
+        diffOutputPath,
+      }) => {
+        if (!pass && !added && !updated) {
+          const message = diffSize
+            ? `Image size (${imageDimensions.baselineWidth}x${
+                imageDimensions.baselineHeight
+              }) different than saved snapshot size (${
+                imageDimensions.receivedWidth
+              }x${
+                imageDimensions.receivedHeight
+              }).\nSee diff for details: ${diffOutputPath}`
+            : `Image was ${diffRatio *
+                100}% different from saved snapshot with ${diffPixelCount} different pixels.\nSee diff for details: ${diffOutputPath}`;
+
+          if (failOnSnapshotDiff) {
+            throw new Error(message);
+          } else {
+            Cypress.log({ message });
           }
         }
-      );
+      }
+    );
   };
 }
 
